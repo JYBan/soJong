93ff1cdc39ba0baea1563ac8ded4b0845089e362
ban <lune0409ify@gmail.com>
2015-11-08 22:52:57 +0900

    form 부분 적당히 해놓음

--- __init__.py
+++ __init__.py
@@ -10,32 +10,51 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'ban'
 
+
+
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
+# MAIN PAGE : SETTING
 @app.route('/', methods=['GET', 'POST'])
-#@app.route('/<sensorList>', methods=['GET', 'POST'])
 def upload_file():
-	sensorList=[]
-	dirs = os.listdir('/Users/Ban/Documents/STUDY/SoftwareCapstoneDesign/workspace/flasktest/soJong/uploads/')
-	for file in dirs:
-		if(file[0]!='.'):
-			sensorList.append(file)
+
+	# upload file
 	if request.method =='POST':
 		file = request.files['file']
-		#return file.filename 
-		#loggin.debug('uploading file '+file.filename)
 		if file and allowed_file(file.filename):
-			#content = file.read()
-			#print content
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
 			flash('File Uploaded')
-			#return redirect(url_for('uploaded_file', filename=filename))
-	return render_template('/index.html', sensorList=sensorList)
+
+	# get file list
+	sensorList=[]
+	dirs = os.listdir('/Users/Ban/Documents/STUDY/SoftwareCapstoneDesign/workspace/flasktest/soJong/uploads/')
+	for file in dirs:
+		if file[0]!='.':
+			sensorList.append(file)
+
+	return render_template('setting.html', sensorList=sensorList)
+
+
+
+# SHOW RESULT
+@app.route('/result', methods=['POST'])
+def show_result():
+	if request.method == 'POST':
+		algo = request.form.getlist('algoCheck')
+		# algo : two of u'hoeffdling', u'sgd' and u'naive' are in list. algo[0] = orange, algo[1] = gray
+		sensor = request.form.get('sensorCheck')
+		# sensor : It's not a list.
+ 		return render_template('showResult.html');
+ 	return render_template('error.html'), 404
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
 if __name__ == '__main__':
 	app.run(debug=True)
